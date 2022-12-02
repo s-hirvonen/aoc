c0af9c69ad863ba3de90b63d484c887fbcc9add4
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-02 12:27:26 +0200

    day 02

--- 2022/Day02.fsx
+++ 2022/Day02.fsx
@@ -10,14 +10,22 @@ type Result = Draw = 0 | PlayerWin = 1 | OpponentWin = 2
 let toChoice = function
     | "A" | "X" -> Choice.Rock
     | "B" | "Y" -> Choice.Paper
-    | _ -> Choice.Scissors
+    | "C" | "Z" | _ -> Choice.Scissors
 
 let result (player: Choice) (opponent: Choice): Result = EnumOfValue ((v player - v opponent) %! 3)
-let score (opponent, player) =
+let choiceFromResult (opponent: Choice) (result: Result): Choice = EnumOfValue ((v opponent + v result) %! 3)
+let score ([|opponent; player|]) =
     match (result player opponent) with
     | Result.PlayerWin -> 6 + v player + 1
     | Result.OpponentWin -> 0 + v player + 1
     | _ -> 3 + v player + 1
 
+let toResult = function
+    | "X" -> Result.OpponentWin
+    | "Y" -> Result.Draw
+    | "Z" | _ -> Result.PlayerWin
 
-let part1 = "2022/inputs/day02.txt" |> System.IO.File.ReadAllLines |> Seq.map (split " " >> (fun ([|a;b|]) -> (toChoice a, toChoice b)) >> score) |> Seq.reduce (+)
+
+let input = "2022/inputs/day02.txt" |> System.IO.File.ReadAllLines |> Seq.map (split " ")
+let part1 = input |> Seq.map (Array.map toChoice >> score) |> Seq.reduce (+)
+let part2 = input |> Seq.map ((fun row -> [| toChoice row.[0]; choiceFromResult (toChoice row.[0]) (toResult row.[1])|]) >> score) |> Seq.reduce (+)
