bdd8b43d498455dc89d2628fc12c3023dc59920c
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-02 17:12:10 +0200

    day 02

--- 2022/Day02.fsx
+++ 2022/Day02.fsx
@@ -23,5 +23,5 @@ let toResult = function
     | "Z" | _ -> int Result.PlayerWin
 
 let input = "2022/inputs/day02.txt" |> System.IO.File.ReadAllLines |> Seq.map (split " ")
-let part1 = input |> Seq.map ((fun row -> (toChoice row.[0], toChoice row.[1])) >> score) |> Seq.reduce (+)
-let part2 = input |> Seq.map ((fun row -> (toChoice row.[0], choiceFromResult (toChoice row.[0]) (toResult row.[1]))) >> score) |> Seq.reduce (+)
+let part1 = input |> Seq.sumBy ((fun row -> (toChoice row.[0], toChoice row.[1])) >> score) 
+let part2 = input |> Seq.sumBy ((fun row -> (toChoice row.[0], choiceFromResult (toChoice row.[0]) (toResult row.[1]))) >> score)
