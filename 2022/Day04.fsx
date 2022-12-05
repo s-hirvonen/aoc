76b71d666bbcd730c115228e5fd48400dc534112
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-05 20:55:03 +0200

    day 04

--- 2022/Day04.fsx
+++ 2022/Day04.fsx
@@ -1,6 +1,4 @@
-open System.Text.RegularExpressions
-
-let split (pattern: string) (str: string) = (Regex pattern).Split(str) |> Array.toList
+let split (delim: char[]) (str: string) = str.Split(delim, System.StringSplitOptions.None) |> Array.toList
 let fullOverlaps = function
     | [a;b;c;d] when a <= c && b >= d -> 1
     | [a;b;c;d] when a >= c && b <= d -> 1
@@ -8,6 +6,6 @@ let fullOverlaps = function
 let overlaps = function
     | [a;b;c;d] when b >= c && a <= d -> 1
     | _ -> 0
+let run input check = input |> Seq.sumBy (split [|','; '-'|] >> List.map int >> check)
 
-let run input check = input |> Seq.sumBy (split "[,-]" >> List.map int >> check)
-[fullOverlaps; overlaps] |> List.map (run ("2022/inputs/day04.txt" |> System.IO.File.ReadAllLines))
+[fullOverlaps; overlaps] |> List.map ("2022/inputs/day04.txt" |> System.IO.File.ReadAllLines |> run)
