df48ef0ac5113fbe181a782798caff578b1cda13
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-05 20:54:52 +0200

    day 01

--- 2022/Day01.fsx
+++ 2022/Day01.fsx
@@ -1,5 +1,5 @@
 let split (delim: string) (str: string) = str.Split(delim, System.StringSplitOptions.RemoveEmptyEntries)
 let input = "2022/inputs/day01.txt" |> System.IO.File.ReadAllText |> split "\n\n" |> Seq.map (split "\n" >> Seq.sumBy int)
 
-let part1 = input |> Seq.max
-let part2 = input |> Seq.sortDescending |> Seq.truncate 3 |> Seq.reduce (+)
+// [part1; part2]
+[1; 3] |> List.map (fun count -> input |> Seq.sortDescending |> Seq.truncate count |> Seq.reduce (+))
