21dd44ad35bdb651547cf0c27ba8ce0d1222c247
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-10 16:32:50 +0200

    day 08 refactor

--- 2022/Day08.fsx
+++ 2022/Day08.fsx
@@ -1,9 +1,9 @@
 let input = System.IO.File.ReadAllLines "2022/inputs/day08.txt" |> Seq.map (Seq.map (fun ch -> int ch - int '0')) |> array2D
 
 let isVisible row i value =
-    let left = [-1] @ (List.take i row)
-    let right = List.skip (i + 1) row @ [-1]
-    (List.forall ((>)value) left) || (List.forall ((>)value) right)
+    let left = (Seq.take i row)
+    let right = Seq.skip (i + 1) row
+    (Seq.forall ((>)value) left) || (Seq.forall ((>)value) right)
 
 let countVisible row i value =
     let visible = Seq.takeWhile ((>)value) >> Seq.length >> (+)1
@@ -12,7 +12,7 @@ let countVisible row i value =
     left * right
 
 let solve operation combinator = 
-    Array2D.mapi (fun y x i -> combinator (operation (List.ofArray input.[*,x]) y i) (operation (List.ofArray input.[y,*]) x i))
+    Array2D.mapi (fun y x i -> combinator (operation input.[*,x] y i) (operation input.[y,*] x i))
 
 input |> (solve isVisible   (||)) |> (Seq.cast<bool> >> Seq.filter id >> Seq.length), // part 1
 input |> (solve countVisible (*)) |> (Seq.cast<int>  >> Seq.max)                      // part 2
