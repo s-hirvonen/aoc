c494c1104a02aa16788d06b087fb64c09e9122f6
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-10 22:52:48 +0200

    day 08 refactor

--- 2022/Day08.fsx
+++ 2022/Day08.fsx
@@ -1,6 +1,6 @@
-let input = System.IO.File.ReadAllLines "2022/inputs/day08.txt" |> Seq.map (Seq.map (System.Char.ToString >> int)) |> array2D
+let input = System.IO.File.ReadAllLines "2022/inputs/day08.txt" |> Seq.map (Seq.map System.Char.ToString) |> array2D
 
-let isVisible row i value = [Seq.take i row; Seq.skip (i + 1) row] |> Seq.map (Seq.forall ((>)value))
+let isVisible row i value = [Seq.take i row; Seq.skip (i + 1) row] |> List.map (Seq.forall ((>)value))
 
 let countVisible row i value =
     let visible = Seq.takeWhile ((>)value) >> Seq.length >> (+)1
@@ -8,7 +8,7 @@ let countVisible row i value =
      row |> Seq.skip (i + 1)      |> visible |> min ((Seq.length row) - i - 1)]
 
 let solve operation combinator = 
-    Array2D.mapi (fun y x i -> [(operation input.[*,x] y i); (operation input.[y,*] x i)] |> Seq.concat |> Seq.reduce combinator)
+    Array2D.mapi (fun y x i -> operation input.[*,x] y i @ operation input.[y,*] x i |> Seq.reduce combinator)
 
-input |> (solve isVisible   (||)) |> (Seq.cast<bool> >> Seq.filter id >> Seq.length), // part 1
-input |> (solve countVisible (*)) |> (Seq.cast<int>  >> Seq.max)                      // part 2
+input |> solve isVisible   (||) |> (Seq.cast<bool> >> Seq.filter id >> Seq.length), // part 1
+input |> solve countVisible (*) |> (Seq.cast<int>  >> Seq.max)                      // part 2
