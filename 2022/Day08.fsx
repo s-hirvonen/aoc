e17298f0b91717c031575b5593a30452d3e86d1c
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-10 22:29:22 +0200

    day 08 refactor

--- 2022/Day08.fsx
+++ 2022/Day08.fsx
@@ -1,18 +1,14 @@
-let input = System.IO.File.ReadAllLines "2022/inputs/day08.txt" |> Seq.map (Seq.map (fun ch -> int ch - int '0')) |> array2D
+let input = System.IO.File.ReadAllLines "2022/inputs/day08.txt" |> Seq.map (Seq.map (System.Char.ToString >> int)) |> array2D
 
-let isVisible row i value =
-    let left = (Seq.take i row)
-    let right = Seq.skip (i + 1) row
-    (Seq.forall ((>)value) left) || (Seq.forall ((>)value) right)
+let isVisible row i value = [Seq.take i row; Seq.skip (i + 1) row] |> Seq.map (Seq.forall ((>)value))
 
 let countVisible row i value =
     let visible = Seq.takeWhile ((>)value) >> Seq.length >> (+)1
-    let left  = row |> Seq.take i |> Seq.rev |> visible |> min i
-    let right = row |> Seq.skip (i + 1)      |> visible |> min ((Seq.length row) - i - 1)
-    left * right
+    [row |> Seq.take i |> Seq.rev |> visible |> min i;
+     row |> Seq.skip (i + 1)      |> visible |> min ((Seq.length row) - i - 1)]
 
 let solve operation combinator = 
-    Array2D.mapi (fun y x i -> combinator (operation input.[*,x] y i) (operation input.[y,*] x i))
+    Array2D.mapi (fun y x i -> [(operation input.[*,x] y i); (operation input.[y,*] x i)] |> Seq.concat |> Seq.reduce combinator)
 
 input |> (solve isVisible   (||)) |> (Seq.cast<bool> >> Seq.filter id >> Seq.length), // part 1
 input |> (solve countVisible (*)) |> (Seq.cast<int>  >> Seq.max)                      // part 2
