27d2f7043a153a99268701bf3a5c6b6ae247b29e
Samuli Hirvonen <hirvonensamuli@gmail.com>
2022-12-22 17:26:08 +0200

    day 16 wip

--- 2022/Day16.fsx
+++ 2022/Day16.fsx
@@ -10,33 +10,46 @@ let parse row =
         | _ -> (node, (rate, connections))
     inner ("", 0, []) row
 
-let part1 (valves: Map<string, int * string list>) =
-    let isOpen valves valve = valves |> Set.contains valve
-    let shouldOpenValve (openedValves, current) = not (isOpen openedValves current) && fst valves[current] > 0
-    let mutable cache = Map.empty<string * string Set * int, int> // (current, openedValves, time) => solution
+let changeItem fromItem toItem l =
+    let index = l |> List.findIndex ((=)fromItem)
+    l |> List.insertAt index toItem |> List.removeAt (index + 1)
 
-    let rec solve state =
-        let moveTo (_, openedValves, time, flowRate) destination = solve (destination, openedValves, time - 1, flowRate)
-        let openValve (current, openedValves, time, flowRate) =
-            solve (current, (Set.add current openedValves), time - 1, flowRate + (fst valves[current]))
+let solve initialState (valves: Map<string, int * string list>) =
+    let shouldOpenValve openedValves current = not (openedValves |> Set.contains current) && fst valves[current] > 0
 
-        let (current, openedValves, time, flowRate) = state
+    let mutable cache = Map.empty<string Set * string Set * int, int> // (current, openedValves, time) => solution
+    let mutable bestSoFar = 0
+
+    let rec solve (state: (string list * string Set * int * int)) =
+        let (currents, openedValves, time, flowRate) = state
+        let moveTo (currents, openedValves, time, flowRate) (from:string) destination = solve (currents |> changeItem from destination, openedValves, time - 1, flowRate)
+        let openValve (currents, openedValves, time, flowRate) valveToOpen =
+            solve (currents, (Set.add valveToOpen openedValves), time - 1, flowRate + (fst valves[valveToOpen]))
+
         // If we have seen this situation before, return precomputed
-        match Map.tryFind (current, openedValves, time) cache with
+        match Map.tryFind (currents |> Set.ofSeq, openedValves, time) cache with
         | Some solution -> solution | None -> (
-
             match time with
             | 0 -> 0 | _ -> (
-                let openOutcome =  if shouldOpenValve (openedValves, current) then [openValve state] else []
-                let moveOutcomes = if openOutcome.Length = 0 then (valves[current] |> snd |> List.map (moveTo state)) else []
-                let optimal = openOutcome @ moveOutcomes |> Seq.reduce max
-                cache <- cache |> Map.add (current, openedValves, time) (optimal + flowRate)
+                // For all possible decisions, recurse on all possible outcomes
+                let openings = currents |> Seq.map (shouldOpenValve openedValves)
+
+                let outcomes = currents |> List.collect (fun current ->
+                    let openOutcome =  if shouldOpenValve openedValves current then [openValve state current] else []
+                    let moveOutcomes = if openOutcome.Length = 0 then valves[current] |> snd |> List.map (moveTo state current) else []
+                    openOutcome @ moveOutcomes)
+
+                let optimal = outcomes |> Seq.reduce max
+                // if optimal > bestSoFar then printfn "%d" optimal
+                bestSoFar <- max optimal bestSoFar
+                cache <- cache |> Map.add (currents |> Set.ofSeq, openedValves, time) (optimal + flowRate)
                 optimal + flowRate))
 
-    solve ("AA", Set.empty, 30, 0)
+    solve initialState
 
 "2022/inputs/day16.txt"
 |> System.IO.File.ReadAllLines
 |> Seq.map (split [|" ";"=";";";","|] >> List.ofArray >> parse)
 |> Map.ofSeq
-|> part1
+|> solve (["AA"], Set.empty, 30, 0)
+// |> solve (["AA"; "AA"], Set.empty, 26, 0)
