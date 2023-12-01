60e10da632d85370d5d8f4bec8c3551d47f16f93
Samuli Hirvonen <hirvonensamuli@gmail.com>
2023-12-01 15:52:30 +0200

    day 01 cleanup

--- 2023/Day01.fsx
+++ 2023/Day01.fsx
@@ -1,17 +1,16 @@
 let numPosition (defs: string list) (str: string) =
     defs
-    |> List.mapi (fun i num -> [ i % 10, str.IndexOf(num); i % 10, str.LastIndexOf(num) ])
-    |> List.concat
-    |> List.filter (snd >> (<=) 0)
+    |> Seq.mapi (fun i num -> [ i % 10, str.IndexOf(num); i % 10, str.LastIndexOf(num) ])
+    |> Seq.concat
+    |> Seq.filter (snd >> (<=) 0)
 
 let solve defs =
     "inputs/day01.txt"
     |> System.IO.File.ReadAllLines
     |> Seq.sumBy (
-        numPosition (defs @ (([0..9] |> List.map string)))
-        >> (fun row -> (List.minBy snd row), (List.maxBy snd row))
-        >> fun (a, b) -> 10 * (fst a % 10) + (fst b % 10)
+        defs @ ([0..9] |> List.map string) |> numPosition
+        >> fun row -> Seq.minBy snd row, Seq.maxBy snd row
+        >> fun (a, b) -> fst a % 10 * 10 + fst b % 10
     )
 
-let part1 = solve []
-let part2 = solve ["zero"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"]
+printfn "%d, %d" <|| (solve [], solve ["zero"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"])
