e5bb7c53c609c30629720758df3ff4a00bc7f18f
Samuli <hirvonensamuli@gmail.com>
2023-12-01 14:16:34 +0200

    day 01

--- 2023/Day01.fsx
+++ 2023/Day01.fsx
@@ -1,11 +1,18 @@
-let nums = ["zero"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"] @ ([ 0..9 ] |> List.map string)
-let numPosition (str: string) =
-    nums |> List.map ((str.IndexOf)) |> List.indexed |> List.filter (snd >> (<=) 0)
+let input = "inputs/day01.txt" |> System.IO.File.ReadAllLines
 
-"inputs/day01.txt"
-|> System.IO.File.ReadLines
-|> Seq.sumBy (
-    numPosition
-    >> (fun row -> (List.minBy snd row), (List.maxBy snd row))
-    >> fun (a, b) -> 10 * (fst a % 10) + (fst b % 10)
-)
+let numPosition (defs: string list) (str: string) =
+    defs
+    |> List.mapi (fun i num -> [ i % 10, str.IndexOf(num); i % 10, str.LastIndexOf(num) ])
+    |> List.concat
+    |> List.filter (snd >> (<=) 0)
+
+let solve defs =
+    input
+    |> Seq.sumBy (
+        numPosition (defs @ (([0..9] |> List.map string)))
+        >> (fun row -> (List.minBy snd row), (List.maxBy snd row))
+        >> fun (a, b) -> 10 * (fst a % 10) + (fst b % 10)
+    )
+
+let part1 = solve []
+let part2 = solve ["zero"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"] 
