f8224467dca8063453844cdf0b2c5b29d9f1cd8e
Samuli Hirvonen <hirvonensamuli@gmail.com>
2023-12-07 20:40:52 +0200

    day 07

---
+++ 2023/Day07.fsx
@@ -0,0 +1,49 @@
+type Hand = | HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind
+
+let split (delim: string) (str: string) = str.Split(delim, System.StringSplitOptions.RemoveEmptyEntries)
+let toPattern = Seq.countBy id >> Seq.map snd >> Seq.sort >> List.ofSeq
+
+let toHand =
+    function
+    | [ 5 ] -> FiveOfAKind
+    | [ 1; 4 ] -> FourOfAKind
+    | [ 2; 3 ] -> FullHouse
+    | [ 1; 1; 3 ] -> ThreeOfAKind
+    | [ 1; 2; 2 ] -> TwoPair
+    | [ 1; 1; 1; 2 ] -> OnePair
+    | _ -> HighCard
+
+let toJokerHand =
+    function
+    | l when List.sum l = 5 -> toHand l
+    | l when List.length l <= 1 -> FiveOfAKind
+    | 1 :: _ :: [] -> FourOfAKind
+    | [ 2; 2 ] -> FullHouse
+    | [ 1; 1; 1 ]
+    | [ 1; 1; 2 ] -> ThreeOfAKind
+    | _ -> OnePair
+
+let valueOf partNum hand =
+    hand
+    |> Seq.map (function
+        | 'T' -> 10
+        | 'J' -> 22 - (11 * partNum)
+        | 'Q' -> 12
+        | 'K' -> 13
+        | 'A' -> 14
+        | n -> int n - int '0')
+    |> Seq.fold (fun acc n -> (15 * acc) + n) 0
+
+let solve partNum =
+    let patternFn = [ toPattern >> toHand; Seq.filter (fun ch -> ch <> 'J') >> toPattern >> toJokerHand ].[partNum - 1]
+
+    System.IO.File.ReadAllLines "inputs/day07.txt"
+    |> Seq.map (
+        split " "
+        >> (fun row -> (patternFn row.[0], valueOf partNum row.[0]), int row.[1]))
+    |> Seq.sortBy fst
+    |> Seq.map snd
+    |> Seq.indexed
+    |> Seq.sumBy (fun (rank, bid) -> (rank + 1) * bid)
+
+printfn $"{solve 1}, {solve 2}"
