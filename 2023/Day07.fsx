5d9ac05469e8ce0bd4d8239df438a6661355781e
Samuli Hirvonen <hirvonensamuli@gmail.com>
2023-12-08 10:41:31 +0200

    day 07 cleanup

--- 2023/Day07.fsx
+++ 2023/Day07.fsx
@@ -1,35 +1,23 @@
 type Hand = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind
 
 let split (delim: string) (str: string) = str.Split(delim, System.StringSplitOptions.RemoveEmptyEntries)
-let toPattern = Seq.countBy id >> Seq.map snd >> Seq.sort >> List.ofSeq
+let toPattern = Seq.countBy id >> Seq.map snd >> Seq.sortDescending >> List.ofSeq
 
-let toHand =
-    function
-    | [ 5 ] -> FiveOfAKind
-    | [ 1; 4 ] -> FourOfAKind
-    | [ 2; 3 ] -> FullHouse
-    | [ 1; 1; 3 ] -> ThreeOfAKind
-    | [ 1; 2; 2 ] -> TwoPair
-    | [ 1; 1; 1; 2 ] -> OnePair
+let toHand = function
+    | [ 5 ]       -> FiveOfAKind  | [ 4; 1 ]    -> FourOfAKind | [ 3; 2 ] -> FullHouse
+    | [ 3; 1; 1 ] -> ThreeOfAKind | [ 2; 2; 1 ] -> TwoPair     | [ 2; 1; 1; 1 ] -> OnePair
     | _ -> HighCard
 
-let toJokerHand =
-    function
+let toJokerHand = function
+    | [] -> FiveOfAKind
     | l when List.sum l = 5 -> toHand l
-    | l when List.length l <= 1 -> FiveOfAKind
-    | 1 :: _ :: [] -> FourOfAKind
-    | [ 2; 2 ] -> FullHouse
-    | [ 1; 1; _ ] -> ThreeOfAKind
-    | _ -> OnePair
+    | l -> toHand (l.Head + 5 - (List.sum l) :: l.Tail)
 
 let valueOf partNum =
     Seq.map (function
-        | 'T' -> 10
-        | 'J' -> 22 - (11 * partNum)
-        | 'Q' -> 12
-        | 'K' -> 13
-        | 'A' -> 14
-        | n -> int n - int '0')
+        | 'T' -> 10 | 'J' -> 22 - (11 * partNum)
+        | 'Q' -> 12 | 'K' -> 13
+        | 'A' -> 14 | n -> int n - int '0')
     >> Seq.fold (fun acc n -> (15 * acc) + n) 0
 
 let solve partNum =
