13f097cee14ed116d9e1789cfa173d50e7bd50fc
Samuli Hirvonen <hirvonensamuli@gmail.com>
2025-12-09 09:11:18 +0200

    day 03

---
+++ 2025/Day03.fsx
@@ -0,0 +1,17 @@
+let rec greatestNDigitStr n xs =
+    if n = 0 then "" else
+        let i, d =
+            xs
+            |> Seq.indexed
+            |> Seq.take (Seq.length xs - n + 1)
+            |> Seq.maxBy snd
+
+        string d
+        + greatestNDigitStr (n - 1) (Seq.skip (i + 1) xs)
+
+let solve n =
+    "2025/inputs/day03.txt"
+    |> System.IO.File.ReadAllLines
+    |> Array.sumBy (Seq.map string >> greatestNDigitStr n >> int64)
+
+[ 2; 12 ] |> List.map (solve >> printfn "%d")
